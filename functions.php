5b08bdcea9566b1f14b4dfff9ad7df19e7154e58
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-24 14:16:42 -0800

    add thumbnails and open links into new tab

--- functions.php
+++ functions.php
@@ -6,40 +6,40 @@ $projects = array(
     array(
         'title' => 'UME Academy',
         'url' => 'https://ume.van.cp.academy.red/',
-        'thumbnail' => 'images/profile.jpg'
+        'thumbnail' => 'images/ume-thumbnail.png'
     ),
     array(
         'title' => 'Quotes on Dev',
         'url' => '#',
-        'thumbnail' => 'images/profile.jpg'
+        'thumbnail' => 'images/quotesondev-thumbnail.png'
     ),
     array(
         'title' => 'Inhabitent',
         'url' => '#',
-        'thumbnail' => 'images/profile.jpg'
+        'thumbnail' => 'images/inhabitent-thumbnail.png'
     ),
     array(
         'title' => 'Pong',
-        'url' => '#',
-        'thumbnail' => 'images/profile.jpg'
+        'url' => 'https://jennapegg.github.io/Pong/',
+        'thumbnail' => 'images/pong-thumbnail.png'
     ),
     array(
         'title' => 'Instanews',
         'url' => 'https://jennapegg.github.io/instanews/',
-        'thumbnail' => 'images/profile.jpg'
+        'thumbnail' => 'images/instanews-thumbnail.png'
     ),
     array(
         'title' => 'Aloha',
-        'url' => '#',
-        'thumbnail' => 'images/profile.jpg'
+        'url' => 'https://jennapegg.github.io/Aloha/',
+        'thumbnail' => 'images/aloha-thumbnail.png'
     ),
 );
 ?>
 <?php foreach ($projects as $project):?>
     <div class="gallery">
-        <a href="<?php print_r($project['url']); ?>">
+        <a href="<?php print_r($project['url']); ?>" target="_blank">
             <p><?php print_r($project['title']); ?></p>
-            <img src="<?php print_r($project['thumbnail']); ?>" alt="Project Thumbnail">
+            <img src="<?php print_r($project['thumbnail']); ?>" alt="Website Thumbnail">
         </a>
     </div>
 <?php endforeach; 
