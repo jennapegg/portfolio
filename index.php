7dfe17c2eafc12ce3cf99350f6b48991586007a5
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-29 21:59:04 -0800

    add new photo and background, restyle header/name

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ include('header.php');
   <main>
     <section class="about">
       <div class="name">
-          <h1>Jenna Pegg</h1>
-          <img src="images/profile.jpg" alt="Jenna Pegg headshot">
+        <img src="images/headshot.jpg" alt="Jenna Pegg headshot">
+          <div class="description">
+            <h1>Jenna Pegg</h1>
+            <p>Web Developer</p>
+          </div>
       </div>
-      <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position. I have completed the Web Development Diploma program at RED Academy, and through this have an excellent understanding of JavaScript, jQuery, PHP, CSS/SCSS, HTML, and more. I bring to a team a positive outlook and work ethic, and an ability to work efficiently both in a group setting and autonomously. </p>
+      <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position.</p>
       <p><a href="./about.php">Read More</a></p>
     </section>
     <section>
