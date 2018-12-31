8fe6af9a99dcbc0e52f91a6e5fcfb6656fe427d4
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-30 17:31:13 -0800

    add style to footer

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
     <div class="contact">
       <p><i class="fas fa-phone-square"></i>(604) 653-8696</p>
       <p><i class="fas fa-envelope-square"></i>jenna.pegg.vic@gmail.com</p>
+    </div>
+    <div class="links">
       <p><i class="fab fa-github-square"></i><a href="https://github.com/jennapegg">GitHub</a></p>
       <p><i class="fab fa-linkedin"></i><a href="https://linkedin.com/in/jenna-pegg">LinkedIn</a></p>
     </div>
