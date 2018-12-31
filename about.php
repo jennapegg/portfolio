fb83945f8685749aee9ceacb2e8e20cfec1c68d2
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-30 17:51:54 -0800

    add style to aboutname

--- about.php
+++ about.php
@@ -1,8 +1,8 @@
 <?php include('header.php') ?>
 <main>
-    <div class="name">
-        <h1>Jenna Pegg</h1>
-        <img src="images/profile.jpg" alt="Jenna Pegg headshot">
+    <div class="aboutname">
+        <h1>About Me</h1>
+        <img src="images/headshot.jpg" alt="Jenna Pegg headshot">
     </div>
     <div class="info">
         <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position. I have completed the Web Development Diploma program at RED Academy, and through this have an excellent understanding of JavaScript, jQuery, PHP, CSS/SCSS, HTML, and more. I bring to a team a positive outlook and work ethic, and an ability to work efficiently both in a group setting and autonomously. </p>
