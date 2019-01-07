4b677d067d1c335e427f7b6d8c00ce6a1fe4151c
Jenna Pegg <jenna.pegg.vic@gmail.com>
2019-01-07 12:29:52 -0800

    add spacing and background to .indexaboutt, and add background image to .seeprojects

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 include('header.php');
+include('functions.php')
 ?>
   <main>
-    <section>
       <div class="indexname">
         <img src="images/headshot.jpg" alt="Jenna Pegg headshot">
           <div class="description">
@@ -10,13 +10,15 @@ include('header.php');
             <p>Web Developer</p>
           </div>
       </div>
-      <div class="about">
-        <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position.</p>
-        <p><a href="./about.php">Read More</a></p>
-      </div>
-    </section>
-    <section>
-      <p><a href="./projects.php">See my Work</a></p>
+      <section class ="index">
+        <div class="indexabout">
+          <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position.</p>
+          <p><a href="./about.php">Read More</a></p>
+          <div class="seeprojects">
+            <?php //get_projects(); ?>
+            <p><a href="./projects.php">See my Work</a></p>
+          </div>
+        </div>
     </section>
   </main>
 <?php include('footer.php') ?>
