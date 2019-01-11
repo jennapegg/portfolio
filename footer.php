2f8ad91d4b412743e106327251ce7212c8098274
Jenna Pegg <jenna.pegg.vic@gmail.com>
2019-01-10 18:32:48 -0800

    adjust mobile footer for easier access

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="contact">
-      <p><i class="fas fa-phone-square"></i><a href="callto:6046538696">(604) 653-8696</a></p>
       <p><i class="fas fa-envelope-square"></i><a href="mailto:jenna.pegg.vic@gmail.com">jenna.pegg.vic@gmail.com</a></p>
+      <p><i class="fas fa-phone-square"></i><a href="callto:6046538696">(604) 653-8696</a></p>
     </div>
     <div class="links">
       <p><i class="fab fa-github-square"></i><a href="https://github.com/jennapegg">GitHub</a></p>
