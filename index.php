a6a9b4f32e2ec08845913616fea26e3c9818579f
Jenna Pegg <jenna.pegg.vic@gmail.com>
2019-01-10 17:37:31 -0800

    add tablet view to indexabout

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include('functions.php')
       <section class ="index">
         <div class="indexabout">
           <div>
-            <p>I am an archaeology student turned web developer ready to take all of the skills learned from both career paths to my next adventure: a full time web development position.</p>
+            <p>I have completed the Web Development Diploma program at RED Academy, and through this have an excellent understanding of JavaScript, jQuery, PHP, CSS/SCSS, HTML, and more. I bring to a team a positive outlook and work ethic, and an ability to work efficiently both in a group setting and autonomously.</p>
             <p><a href="./about.php">Read More</a></p>
           </div>
           <div class="seeprojects">
